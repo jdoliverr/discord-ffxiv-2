ca37b9ef38824413c5457d4a593d5ba3dd4c3456
Jon <jdog992@gmail.com>
2023-04-13 21:16:55 -0400

    updated gnb and mnk sets

--- commands/gnb.js
+++ commands/gnb.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('2.40: https://etro.gg/gearset/88fbea7d-3b43-479c-adb8-b87c9d6cb5f9 \n2.41: https://etro.gg/gearset/c2962f7b-ea07-4aa9-9b84-21eb8ac03d90 \n2.42: https://etro.gg/gearset/0c777bae-1680-4b0e-ac7e-56217df5611e \n2.43: https://etro.gg/gearset/e863ace7-ffcb-4c3d-b8de-8038f99a7308 \n2.44: https://etro.gg/gearset/b8d9ed19-8b42-41de-b414-ee83ed799416 \n2.45: https://etro.gg/gearset/d6f564f3-e918-4dc8-8bf7-95764ada60a7 \n2.46: https://etro.gg/gearset/a69c29ce-50f2-4fd2-9db3-6a4704c707d8 ');} else {
+        if(!member) {message.channel.send('2.50 Standard: https://etro.gg/gearset/3c586380-f796-4502-9621-c9d00816cb81 \n2.50 22 Strength Races: https://etro.gg/gearset/88da32ad-e4e5-4f02-b194-840aaf5cf684 \n2.45 Standard: https://etro.gg/gearset/1cdcf24b-af97-4d6b-ab88-dcfee79f791c \n2.45 22 Strength Races: https://etro.gg/gearset/1300c0c9-cf6b-474c-aba9-f1bfeb31976d \n2.42 Standard: https://etro.gg/gearset/afa282c8-d5b9-41a8-8adf-aca95bc3de33 \n2.42 22 Strength Races: https://etro.gg/gearset/559c9e70-eeea-400d-a71f-7cb4ba304181');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
--- commands/mch.js
+++ commands/mch.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('https://etro.gg/gearset/6b4b1ba5-a821-41a0-b070-b1f50e986f85');} else {
+        if(!member) {message.channel.send('https://etro.gg/gearset/8a0bdf80-80f5-42e8-b10a-160b0fc2d151');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
