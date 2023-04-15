cd47a480993fdc2b2b992fdea5f754c260587164
Jon <jdog992@gmail.com>
2023-04-15 01:22:19 -0400

    updated nin

--- commands/nin.js
+++ commands/nin.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('https://etro.gg/gearset/b9876a4d-aba9-48f0-9c03-cb542af46a29');} else {
+        if(!member) {message.channel.send('2.10: https://etro.gg/gearset/c0c2ba50-b93a-4d18-8cba-a0ebb0705fed \n2.12: https://etro.gg/gearset/9e07d761-8105-4ee5-9921-8f35d8b4a631');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
