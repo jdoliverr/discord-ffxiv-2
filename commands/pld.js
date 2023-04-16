59ec1c59903713a2db91821dcaea7223a485ade5
Jon <jdog992@gmail.com>
2023-04-16 02:16:05 -0400

    updated pld sets

--- commands/pld.js
+++ commands/pld.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('2.45: https://etro.gg/gearset/38fe3778-f2c1-4300-99e4-b58a0445e969 \n2.44: https://etro.gg/gearset/8cbbfbed-e2da-4866-8873-ffba3276b682 \n2.42: https://etro.gg/gearset/bb893548-09aa-4a75-bb2f-90971bdcab38 \n2.40: https://etro.gg/gearset/ca8e2ea6-2e6a-4e69-93b0-711a51db0a27');} else {
+        if(!member) {message.channel.send('2.50: https://etro.gg/gearset/3e6864e6-ea46-402d-96a2-3fe07bb2506b \n2.45: https://etro.gg/gearset/0c8ba3f0-efe4-453f-9b38-16745a11a7b5 \n2.42: https://etro.gg/gearset/8361ed48-9503-449b-9e6c-f446dc8b0382');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
