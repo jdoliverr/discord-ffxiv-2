b6fe81cd912749ed7088e895c93b6dce3ea7fdee
Jon <jdog992@gmail.com>
2023-04-18 15:48:03 -0400

    updated rdm bis

--- commands/rdm.js
+++ commands/rdm.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('2.48: https://etro.gg/gearset/80fdec19-1109-4ca2-8172-53d4dda44144 \n2.50: https://etro.gg/gearset/8c456a46-f3cb-4a23-9771-9f267892bcc0');} else {
+        if(!member) {message.channel.send('2.48: https://etro.gg/gearset/5f972eb8-c3cd-44da-aa73-0fa769957e5b');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
