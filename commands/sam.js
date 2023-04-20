39234c5daeabe88cac49a2b6a92e979d64e30981
Jon <jdog992@gmail.com>
2023-04-20 15:35:57 -0400

    updated sam bis

--- commands/sam.js
+++ commands/sam.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('2.14: https://etro.gg/gearset/3a7c7f45-b715-465d-a377-db458045506a \n2.07: https://etro.gg/gearset/17d99be3-021c-4d6c-a9e3-0504e62c8c1f \n2.00: https://etro.gg/gearset/84c1f493-7f65-4d9e-bd3c-34965c75cf41');} else {
+        if(!member) {message.channel.send('2.15: https://etro.gg/gearset/4356046d-2f05-432a-a98c-632f11098ade \n2.08: https://etro.gg/gearset/56e084de-6194-45a5-8af7-6834f902f791 \n2.00: https://etro.gg/gearset/c7601428-cf57-456b-a2ed-321fd2c25ff7');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
