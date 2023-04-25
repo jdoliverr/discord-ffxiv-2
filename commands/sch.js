7e2a2b13ee771ae874da91c6f3911ad120a71783
Jon <jdog992@gmail.com>
2023-04-25 16:03:13 -0400

    updated sch bis

--- commands/sch.js
+++ commands/sch.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('2.50 Hi Pi: https://etro.gg/gearset/30122448-70c8-421c-bd8c-820e2905858b \n2.41 Mid Pi: https://etro.gg/gearset/f1802c19-d766-40f0-b781-f5b965cb964e \n2.40 Lo Pi: https://etro.gg/gearset/ff3ffc7a-e28c-45bf-a310-a8d8496f8ebf');} else {
+        if(!member) {message.channel.send('2.50 Hi Pi: https://etro.gg/gearset/5e124e66-7cd7-470a-95d8-f71f60811c0f \n2.40 Mid Pi: https://etro.gg/gearset/03f4b5a3-e9ab-434b-8cc2-b358546fcb60 \n2.40 Lo Pi: https://etro.gg/gearset/bb015772-1160-41a5-aa62-7e92d3998bbf');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
