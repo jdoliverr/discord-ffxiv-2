e5221bcf5adfdaefb57c4e1adf002124d8146f56
Jon <jdog992@gmail.com>
2023-04-26 15:52:37 -0400

    updated sge bis

--- commands/sge.js
+++ commands/sge.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async (bot, message, args) => {
         let member = message.mentions.members.first();
-        if(!member) {message.channel.send('Tome ring: https://etro.gg/gearset/3c7d9741-0e74-41d7-9ec4-1b2e7c1673a5 \nCrafted Ring: https://etro.gg/gearset/32c72ae2-3c53-49cf-ac02-b989c693d8b5');} else {
+        if(!member) {message.channel.send('Tome ring: https://etro.gg/gearset/39c93c65-e049-4d2c-9439-4812499f6acc \nCrafted Ring: https://etro.gg/gearset/80bec2f5-8e9e-43fb-adcf-0cd7f7018c02');} else {
             message.channel.send(`Hello ${member.user.tag}`)
         }
 };
